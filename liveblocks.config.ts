0edb43e628f9af1b94dc6410811625401888ae11
MarkSuttirak <suttirak.ch@zaviago.com>
2024-04-05 17:48:33 +0700

    Fix the build

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -1,10 +1,9 @@
 import { createClient, LiveList, LiveMap, LiveObject } from "@liveblocks/client";
 import { createRoomContext, createLiveblocksContext } from "@liveblocks/react";
-import { Color, Layer } from "./types/canvas";
-  
+
 const client = createClient({
   throttle: 16,
-  authEndpoint: "/api/liveblocks-auth",
+  authEndpoint: "",
   async resolveUsers({ userIds }) {
     // Used only for Comments and Notifications. Return a list of user information
     // retrieved from `userIds`. This info is used in comments, mentions etc.
@@ -51,10 +50,6 @@ const client = createClient({
 // and that will automatically be kept in sync. Accessible through the
 // `user.presence` property. Must be JSON-serializable.
 type Presence = {
-  cursor: { x: number, y: number } | null,
-  selection: string[],
-  pencilDraft: [x: number, y:number, pressure: number][] | null,
-  penColor: Color | null
   // ...
 };
 
@@ -63,19 +58,13 @@ type Presence = {
 // LiveList, LiveMap, LiveObject instances, for which updates are
 // automatically persisted and synced to all connected clients.
 type Storage = {
-  layers: LiveMap<string, LiveObject<Layer>>
-  layerIds: LiveList<string>
 };
 
 // Optionally, UserMeta represents static/readonly metadata on each user, as
 // provided by your own custom auth back end (if used). Useful for data that
 // will not change during a session, like a user's name or avatar.
 type UserMeta = {
-  id?: string;
-  info?: {
-    name?: string;
-    picture?: string;
-  }
+
 };
 
 // Optionally, the type of custom events broadcast and listened to in this
