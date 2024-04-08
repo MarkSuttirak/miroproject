bb334025b6699f945f71c3c94170f7d7bb23b56f
MarkSuttirak <suttirak.ch@zaviago.com>
2024-04-08 09:40:31 +0700

    Update convex-client-provider.tsx

--- providers/convex-client-provider.tsx
+++ providers/convex-client-provider.tsx
@@ -13,7 +13,7 @@ const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
 
 export default function ConvexClientProvider({children} : ConvexProviderProps){
     return (
-        <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}>
+        <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
           <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
 
             <Authenticated>
