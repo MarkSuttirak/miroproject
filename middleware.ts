f8cbf61b8ad17d6b362be2e4022eeb442673bc39
MarkSuttirak <suttirak.ch@zaviago.com>
2024-04-08 11:10:13 +0700

    Fix the build

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-    publicRoutes: ['https://miroproject.vercel.app/']
+    publicRoutes: ['/']
 });
 
 export const config = {
